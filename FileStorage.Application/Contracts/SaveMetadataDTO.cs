f659d11c053301535ee7af9ead63664fdba719ea
Sergey <simon.serezha1@yandex.ru>
2025-09-27 19:09:49 +0500

    feat: add dto for file saving

---
+++ FileStorage.Application/Contracts/SaveMetadataDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace FileStorage.Application.Contracts;
+
+public class SaveMetadataDto
+{
+    public Guid Id { get; set; }
+    public string FileName { get; set; } = string.Empty;
+    public string Extension { get; set; } = string.Empty;
+    public long Size { get; set; }
+    public DateTime UploadDate { get; set; }
+}
