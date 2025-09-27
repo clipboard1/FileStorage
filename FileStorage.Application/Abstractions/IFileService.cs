c5e7a5d0697d59b6f5b77165ea69c3686a296388
Sergey <simon.serezha1@yandex.ru>
2025-09-27 19:09:28 +0500

    feat: add services abstractions

---
+++ FileStorage.Application/Abstractions/IFileService.cs
@@ -0,0 +1,8 @@
+﻿namespace FileStorage.Application.Abstractions;
+
+public interface IFileService
+{
+    public Task<bool> SaveFile(FileStream file, CancellationToken cancellation = default);
+    public Task<bool> DeleteFile(Guid id, CancellationToken cancellation = default);
+    public Task<FileStream> GetFile(Guid id, CancellationToken cancellation = default);
+}
