0fa0a4b3609abdcea63783051f6bc63c95d27a70
Sergey <simon.serezha1@yandex.ru>
2025-10-04 15:08:22 +0500

    feat: add dtos for actions with files

---
+++ FileStorage.Application/Contracts/DownloadFileDTO.cs
@@ -0,0 +1,7 @@
+﻿namespace FileStorage.Application.Contracts;
+
+public class DownloadFileDTO
+{
+    public FileStream Stream { get; set; }
+    public string FileName { get; set; }
+}
---
+++ FileStorage.Application/Contracts/FileInfoDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace FileStorage.Application.Contracts;
+
+public class FileInfoDTO
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Extension { get; set; }
+    public long Size { get; set; }
+}
