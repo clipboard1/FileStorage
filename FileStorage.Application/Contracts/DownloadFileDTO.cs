d2113051def3d7c723d44f1714a626a9a7ecb9fc
Sergey <simon.serezha1@yandex.ru>
2025-11-06 20:21:50 +0500

    refactor: remove useless dto's

--- FileStorage.Application/Contracts/DownloadFileDTO.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace FileStorage.Application.Contracts;
-
-public class DownloadFileDTO
-{
-    public FileStream Stream { get; set; }
-    public string FileName { get; set; }
-}
--- FileStorage.Application/Contracts/FileInfoDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FileStorage.Application.Contracts;
-
-public class FileInfoDTO
-{
-    public Guid Id { get; set; }
-    public string Name { get; set; }
-    public string Extension { get; set; }
-    public long Size { get; set; }
-}
