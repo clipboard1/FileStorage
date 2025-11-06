104b49a8d348cc8f6083e054a6651ad6272c4135
Sergey <simon.serezha1@yandex.ru>
2025-11-06 20:27:01 +0500

    feat: register named httpclient, interactions service

--- FileStorage.API/Program.cs
+++ FileStorage.API/Program.cs
@@ -1,9 +1,16 @@
+using FileStorage.API;
 using FileStorage.Application.Abstractions;
 using FileStorage.Application.Services;
+using FileStorage.Infrastructure;
 using FileStorage.Infrastructure.Storage.Abstractions;
 using FileStorage.Infrastructure.Storage.Handlers;
+using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddEnvironmentVariables();
+
+builder.Services.Configure<ActivityServiceSettings>(builder.Configuration.GetSection(nameof(ActivityServiceSettings)));
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -11,7 +18,22 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<IFileHandler, FileHandler>();
 builder.Services.AddScoped<IFileService, FileService>();
 
+builder.Services.AddHttpClient("InteractionsClient", (sp, client) =>
+{
+    var settings = sp.GetRequiredService<IOptions<ActivityServiceSettings>>().Value;
+    client.BaseAddress = new Uri(settings.BaseUrl);
+});
+
+builder.Services.AddScoped<IInteractionsService, InteractionsService>(sp =>
+{
+    var settings = sp.GetRequiredService<IOptions<ActivityServiceSettings>>().Value;
+    var httpClient = sp.GetRequiredService<IHttpClientFactory>().CreateClient("InteractionsClient");
+    return new InteractionsService(httpClient, settings);
+});
+
 builder.Services.AddControllers();
+
+
 
 var app = builder.Build();
 
