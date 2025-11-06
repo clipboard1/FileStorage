81cbe2821a5b2b9d6d1795cc38e2a9f0a135a1e5
Sergey <simon.serezha1@yandex.ru>
2025-11-06 20:22:11 +0500

    feat: add interaction service

---
+++ FileStorage.Application/Abstractions/IInteractionsService.cs
@@ -0,0 +1,9 @@
+﻿using FileStorage.Infrastructure;
+
+namespace FileStorage.Application.Abstractions;
+
+public interface IInteractionsService
+{
+    public Task<Result> StartInteraction(CancellationToken cancellation = default);
+    public Task<Result> EndInteraction(CancellationToken cancellation = default);
+}
---
+++ FileStorage.Application/Services/InteractionsService.cs
@@ -0,0 +1,38 @@
+﻿using System.Net.Http.Json;
+using FileStorage.API;
+using FileStorage.Application.Abstractions;
+using FileStorage.Infrastructure;
+
+namespace FileStorage.Application.Services;
+
+public class InteractionsService : IInteractionsService
+{
+    private readonly HttpClient _httpClient;
+    private readonly ActivityServiceSettings _activitySettings;
+
+    public InteractionsService(HttpClient httpClient, ActivityServiceSettings settings)
+    {
+        _httpClient = httpClient;
+        _activitySettings = settings;
+    }
+
+    public async Task<Result> StartInteraction(CancellationToken cancellation = default)
+    {
+        var response = await _httpClient.PostAsync(_activitySettings.StartInteractionEndpoint, null, cancellation);
+
+        if (!response.IsSuccessStatusCode)
+            return Result.Failure(Result.ToDict("Activity request", response.ReasonPhrase ?? "Something went wrong"));
+
+        return Result.Success();
+    }
+
+    public async Task<Result> EndInteraction(CancellationToken cancellation = default)
+    {
+        var response = await _httpClient.PostAsync(_activitySettings.EndInteractionEndpoint, null, cancellation);
+
+        if (!response.IsSuccessStatusCode)
+            return Result.Failure(Result.ToDict("Activity request", response.ReasonPhrase ?? "Something went wrong"));
+
+        return Result.Success();
+    }
+}
